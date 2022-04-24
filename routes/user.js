ccb0ad1f01f36d7b1234423d7f8d621e9c6d4e1d
Nikkolai Fernandez <nikkolaifernandez@Nikkolais-MacBook-Air.local>
2022-04-24 16:10:05 +0800

    add signup post endpoint

--- routes/user.js
+++ routes/user.js
@@ -3,3 +3,18 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport')
 const user = require(`${path.resolve('./')}/controllers/user`)
+const signupValidations = require(`${path.resolve('./')}/validations/user/signupValidations`);
+
+/**
+ * Register a user 
+ *
+ * @apiParam {String} username username as email 
+ * @apiParam {String} password Password
+ *
+ * @apiSuccess {Boolean} state Success response
+ */
+router.post(
+    '/signup',
+    signupValidations,
+    user.signup
+)
